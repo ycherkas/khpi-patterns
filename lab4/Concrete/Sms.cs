90b0f2005ae83ad162f8d0f5f67df3bb34c5f5e2
Yuriy Cherkas <Yuriy.Cherkas@dataart.com>
2022-10-28 12:43:41 +0300

    Laboratory work 4 - Implementation

---
+++ lab4/Concrete/Sms.cs
@@ -0,0 +1,10 @@
+﻿namespace Laboratory4.Concrete
+{
+    public class Sms
+    {
+        public void SendSms(string senderPhoneNumber, string recepientPhoneNumber, string text)
+        {
+            //TODO: implement
+        }
+    }
+}
---
+++ lab4/Demo.cs
@@ -0,0 +1,37 @@
+﻿using Laboratory4.Concrete;
+using Laboratory4.Interfaces;
+
+namespace Laboratory4
+{
+    public class Demo
+    {
+        private const string LOGIN = "login";
+        private const string EMAIL = "email@gmail.com";
+        private const string SLACK_API_KEY = "key";
+
+        public void Run()
+        {
+            var message = "Hello World!";
+
+            var emailNotification = new EmailNotification(EMAIL);
+            SendMessage(emailNotification, "title", message);
+
+            var slack = new Slack();
+            var slackAdapter = new SlackAdapter(slack);
+            slackAdapter.Authorize(LOGIN, SLACK_API_KEY);
+            slackAdapter.SetChatId("chatId");
+            SendMessage(slackAdapter, "", message);
+
+            var sms = new Sms();
+            var smsAdapter = new SmsAdapter(sms);
+            smsAdapter.SetSenderNumber("+38(050)123-45-67");
+            smsAdapter.SetRecepientNumber("+38(066)987-65-43");
+            SendMessage(smsAdapter, "", message);
+        }
+
+        private void SendMessage(INotification notification, string title, string message)
+        {
+            notification.Send(title, message);
+        }
+    }
+}
