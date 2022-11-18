07d2bb2e9caec27f4e8816d59fc5dc589e617907
Yuriy Cherkas <Yuriy.Cherkas@dataart.com>
2022-11-18 10:29:35 +0200

    Laboratory work 6 - Implementation

---
+++ lab6/Demo.cs
@@ -0,0 +1,20 @@
+﻿using Laboratory6.Concrete;
+using Laboratory6.Interfaces;
+
+namespace Laboratory6
+{
+    public class Demo
+    {
+        public void Run()
+        {
+            IDownloader downloader = new SimpleDownloader();
+            IDownloader cachedDownloader = new CachedDownloader(downloader);
+
+            var client = new Client();
+            client.ProcessDownload(cachedDownloader, "https://site.com/file1");
+            client.ProcessDownload(cachedDownloader, "https://site.com/file2");
+            //file1 is taken from internal cache
+            client.ProcessDownload(cachedDownloader, "https://site.com/file1");
+        }
+    }
+}
